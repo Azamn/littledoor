1706bbcd3ddbe930bdaf6039f20821ec59d236ac
Azam Ali Shaikh <30806300+Azamn@users.noreply.github.com>
2023-12-31 21:00:36 +0530

    privacy policy updating

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -20,15 +21,15 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                        @yield('code')
-                    </div>
-
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        @yield('message')
-                    </div>
+                    @if(!is_null(@$privacyPolicyData))
+                        @foreach ($privacyPolicyData as $policy)
+                            <p>${{ $policy['privacy_policy'] }}</p>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
+
     </body>
+
 </html>
