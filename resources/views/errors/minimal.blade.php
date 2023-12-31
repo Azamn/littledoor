c681f10e8c3ce2a5c6e939cdc94519f8d299730c
Azam Ali Shaikh <30806300+Azamn@users.noreply.github.com>
2023-12-31 21:22:56 +0530

    privacy policy updating

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -535,10 +535,10 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div class="margin:20px mx-auto sm:px-6 lg:px-12">
+        <div class="flex mx-auto sm:px-6 lg:px-12 width: 70%;">
             @if (!is_null(@$privacyPolicyData))
                 @foreach ($privacyPolicyData as $policy)
-                    <p><strong>{{ $policy['privacy_policy'] }} </strong></p> <br>
+                    <p><strong>{{ $policy['privacy_policy'] }} </strong></p>
                 @endforeach
             @endif
         </div>
