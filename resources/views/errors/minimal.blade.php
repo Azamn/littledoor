2c72367ae4e20bdc62febd56d9d80e7a390684b5
Azam Ali Shaikh <30806300+Azamn@users.noreply.github.com>
2024-01-01 00:11:12 +0530

    privacy policy page

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('Admin.layouts.navbar')
-
 <head>
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -618,7 +616,7 @@
                         {{--                            </div> --}}
                     </li>
                 </ul>
-            {{-- </div> --}} 
+            {{-- </div> --}}
             {{-- <div class="nav-right col-8 text-right pull-right right-header p-0">
                 <ul class="nav-menus">
 
