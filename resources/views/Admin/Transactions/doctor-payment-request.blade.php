c90b01b67c5e5e750b91e67e22fd77de5309b12f
Azam Ali Shaikh <30806300+Azamn@users.noreply.github.com>
2024-01-28 01:40:57 +0530

    doctor payment request

--- resources/views/Admin/Transactions/doctor-payment-request.blade.php
+++ resources/views/Admin/Transactions/doctor-payment-request.blade.php
@@ -17,7 +17,6 @@
                 </div>
             </div>
         </div>
-        <! Container-fluid starts>
             <div class="container-fluid">
                 <div class="card">
                     <div class="card-body">
